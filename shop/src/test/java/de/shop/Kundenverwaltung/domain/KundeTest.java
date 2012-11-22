360f9e63963052ef20462debd85011e1e54a6e55
Gliniorz <glja1011(at)hs-karlsruhe.de>
2012-11-22 12:53:47 +0100

    Added more Tests for Kunde + Adresse, Wrote Create-Tests

--- shop/src/test/java/de/shop/Kundenverwaltung/domain/KundeTest.java
+++ shop/src/test/java/de/shop/Kundenverwaltung/domain/KundeTest.java
@@ -17,6 +17,8 @@ import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
 import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.RollbackException;
@@ -42,24 +44,23 @@ public class KundeTest extends AbstractDomainTest {
 	private static final int TAG = 31;
 	private static final int MONAT = Calendar.JANUARY;
 	private static final int JAHR = 2001;
-	private static final Date SEIT_VORHANDEN = new GregorianCalendar(JAHR, MONAT, TAG).getTime();
+	private static final Date ERSTELLT_VORHANDEN = new GregorianCalendar(JAHR, MONAT, TAG).getTime();
 	
 	private static final String NACHNAME_NEU = "Test";
 	private static final String VORNAME_NEU = "Theo";
 	private static final String EMAIL_NEU = "theo@test.de";
-	private static final float RABATT_NEU = 0.5f;
-	private static final double UMSATZ_NEU = 10_001_000;
 	private static final String PLZ_NEU = "11111";
 	private static final String ORT_NEU = "Testort";
 	private static final String STRASSE_NEU = "Testweg";
 	private static final String HAUSNR_NEU = "1";
+	private static final String LAND_NEU = "Deutschland";
 	
 	
 	@Test
 	public void validate() {
 		assertThat(true, is(true));
 	}
-	/*
+	
 	@Test
 	public void findKundeByIdVorhanden() {
 		// Given
@@ -69,7 +70,7 @@ public class KundeTest extends AbstractDomainTest {
 		//final AbstractKunde kunde = getEntityManager().find(AbstractKunde.class, id);
 		
 		Kunde kunde = getEntityManager().createNamedQuery(Kunde.KUNDE_BY_KNR, Kunde.class)
-							.setParameter(Kunde.PARAM_KUNDENNUMMER, 12)
+							.setParameter(Kunde.PARAM_KUNDENNUMMER, id)
 							.getSingleResult();
 				
 				
@@ -113,28 +114,26 @@ public class KundeTest extends AbstractDomainTest {
 		final String nachname = NACHNAME_VORHANDEN;
 		
 		// When
-		final List<AbstractKunde> kunden = getEntityManager().createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_NACHNAME,
-                                                                               AbstractKunde.class)
-                                                            .setParameter(AbstractKunde.PARAM_KUNDE_NACHNAME, nachname)
-				                                            .getResultList();
+		List <Kunde> kunden = getEntityManager().createNamedQuery(Kunde.KUNDE_BY_NACHNAME, Kunde.class)
+						.setParameter(Kunde.PARAM_NACHNAME, nachname)
+						.getResultList();
 		
 		// Then
 		assertThat(kunden.isEmpty(), is(false));
-		for (AbstractKunde k : kunden) {
+		for(Kunde k : kunden){
 			assertThat(k.getNachname(), is(nachname));
 		}
+		
 	}
-	
 	@Test
 	public void findKundenByNachnameNichtVorhanden() {
 		// Given
 		final String nachname = NACHNAME_NICHT_VORHANDEN;
 		
 		// When
-		final List<AbstractKunde> kunden = getEntityManager().createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_NACHNAME,
-                                                                               AbstractKunde.class)
-                                                            .setParameter(AbstractKunde.PARAM_KUNDE_NACHNAME, nachname)
-				                                            .getResultList();
+		List<Kunde> kunden = getEntityManager().createNamedQuery(Kunde.KUNDE_BY_NACHNAME, Kunde.class)
+						.setParameter(Kunde.PARAM_NACHNAME, nachname)
+						.getResultList();
 		
 		// Then
 		assertThat(kunden.isEmpty(), is(true));
@@ -143,45 +142,46 @@ public class KundeTest extends AbstractDomainTest {
 	@Test
 	public void findKundenByDateVorhanden() {
 		// Given
-		final Date seit = SEIT_VORHANDEN;
+		final Date seit = ERSTELLT_VORHANDEN;
 		
 		// When
-		final List<AbstractKunde> kunden = getEntityManager().createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_DATE,
-                                                                               AbstractKunde.class)
-                                                             .setParameter(AbstractKunde.PARAM_KUNDE_SEIT, seit,
-                                                            		       TemporalType.DATE)
-				                                             .getResultList();
 		
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Kunde> criteriaQuery =
+		builder.createQuery(Kunde.class);
+		Root<Kunde> k = criteriaQuery.from(Kunde.class);
+		Path<Date> DatePath = k.get(Kunde_.erstelltAm);
+		// k.get("nachname")
+		Predicate pred = builder.equal(DatePath, seit);
+		criteriaQuery.where(pred);
+		TypedQuery<Kunde>query = getEntityManager().createQuery(criteriaQuery);
+		List<Kunde> kunden = query.getResultList();
+					
 		// Then
 		assertThat(kunden.isEmpty(), is(false));
-		for (AbstractKunde k : kunden) {
-			assertThat(k.getSeit(), is(seit));
+		for (Kunde ku: kunden) {
+			assertThat(ku.getErstelltAm(), is(seit));
 		}
+	
 	}
 
 	@Test
 	public void createPrivatkunde() {
 		// Given
-		Privatkunde kunde = new Privatkunde();
+		Kunde kunde = new Kunde();
 		kunde.setNachname(NACHNAME_NEU);
 		kunde.setVorname(VORNAME_NEU);
 		kunde.setEmail(EMAIL_NEU);
-		kunde.setRabatt(RABATT_NEU);
-		kunde.setUmsatz(UMSATZ_NEU);
-		kunde.setSeit(SEIT_VORHANDEN);
+		kunde.setErstelltAm(ERSTELLT_VORHANDEN);
 		
 		final Adresse adresse = new Adresse();
 		adresse.setPlz(PLZ_NEU);
 		adresse.setOrt(ORT_NEU);
 		adresse.setStrasse(STRASSE_NEU);
-		adresse.setHausnr(HAUSNR_NEU);
+		adresse.setHausNr(HAUSNR_NEU);
+		adresse.setLand(LAND_NEU);
 		adresse.setKunde(kunde);
 		kunde.setAdresse(adresse);
-		
-		kunde.setNewsletter(true);
-		kunde.setGeschlecht(GeschlechtType.WEIBLICH);
-		kunde.setFamilienstand(FamilienstandType.VERHEIRATET);
-		kunde.setHobbies(Arrays.asList(HobbyType.LESEN, HobbyType.SPORT));
 		
 		// When
 		try {
@@ -202,16 +202,14 @@ public class KundeTest extends AbstractDomainTest {
 		// Then
 		
 		// Den abgespeicherten Kunden ueber eine Named Query ermitteln
-		final List<AbstractKunde> kunden = getEntityManager().createNamedQuery(AbstractKunde.FIND_KUNDEN_BY_NACHNAME,
-                                                                               AbstractKunde.class)
-                                                             .setParameter(AbstractKunde.PARAM_KUNDE_NACHNAME,
-                                                            		       NACHNAME_NEU)
-				                                             .getResultList();
+		List<Kunde> kunden = getEntityManager().createNamedQuery(Kunde.KUNDE_BY_NACHNAME, Kunde.class)
+									.setParameter(Kunde.KUNDE_BY_NACHNAME, NACHNAME_NEU)
+									.getResultList();
 		
 		// Ueberpruefung des ausgelesenen Objekts
 		assertThat(kunden.size(), is(1));
-		kunde = (Privatkunde) kunden.get(0);
-		assertThat(kunde.getId().longValue() > 0, is(true));
+		kunde = (Kunde) kunden.get(0);
+		assertThat(kunde.getKundenNr().longValue() > 0, is(true));
 		assertThat(kunde.getNachname(), is(NACHNAME_NEU));
 	}
 	
@@ -220,16 +218,15 @@ public class KundeTest extends AbstractDomainTest {
 	public void notYetImplemented() {
 		fail();
 	}
-
+	
 	@Test
-	public void createPrivatkundeOhneAdresse() throws HeuristicMixedException, HeuristicRollbackException,
-	                                                  SystemException {
+	public void createKundeOhneAdresse() throws HeuristicMixedException, HeuristicRollbackException, SystemException {
 		// Given
 		final String nachname = NACHNAME_NEU;
 		final String email = EMAIL_NEU;
 		
 		// When
-		final Privatkunde kunde = new Privatkunde();
+		final Kunde kunde = new Kunde();
 		kunde.setNachname(nachname);
 		kunde.setEmail(email);
 		getEntityManager().persist(kunde);
@@ -251,5 +248,5 @@ public class KundeTest extends AbstractDomainTest {
 		}
 
 	}
-	*/
+	
 }
