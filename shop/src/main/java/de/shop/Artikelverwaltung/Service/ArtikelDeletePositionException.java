3feacfe7eca1a69e6326037b03e8b9cefb0d49be
Patrick Frey <fpatrick@web.de>
2012-12-11 16:32:29 +0100

    Added Service Folder for Artikelverwaltung

---
+++ shop/src/main/java/de/shop/Artikelverwaltung/Service/ArtikelDeletePositionException.java
@@ -0,0 +1,31 @@
+package de.shop.Artikelverwaltung.Service;
+
+import javax.ejb.ApplicationException;
+
+import de.shop.Artikelverwaltung.domain.Artikel;
+import de.shop.Kundenverwaltung.domain.Kunde;
+
+
+/**
+ * Exception, die ausgel&ouml;st wird, wenn ein Kunde gel&ouml;scht werden soll, aber mindestens eine Bestellung hat
+ */
+@ApplicationException(rollback = true)
+public class ArtikelDeletePositionException extends ArtikelServiceException {
+	private static final long serialVersionUID = 1L;
+	private final Long artikelId;
+	private final int anzahlPositionen;
+	
+	public ArtikelDeletePositionException(Artikel artikel) {
+		super("Artikel mit der ID=" + artikel.getId() + " kann nicht geloescht werden: "
+			  + artikel.getLagerposition().size() + " Position(en)");
+		this.artikelId = artikel.getId();
+		this.anzahlPositionen = artikel.getLagerposition().size();;
+	}
+
+	public Long getArtikelId() {
+		return artikelId;
+	}
+	public int getAnzahlPositionen() {
+		return anzahlPositionen;
+	}
+}
